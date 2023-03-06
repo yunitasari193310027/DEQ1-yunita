817b14645a98d6409ac4741f3395daa27c895118
Yunita Sari <102893382+yunitasari193310027@users.noreply.github.com>
2023-03-06 19:32:46 +0700

    Update missing value.py
    fix

--- Data Quality with Python for Beginner/missing value.py
+++ Data Quality with Python for Beginner/missing value.py
@@ -4,16 +4,14 @@ import io
 import pandas_profiling
 retail_raw = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv')
 
-# Q1, Q3, dan IQR
-Q1 = retail_raw['item_price'].quantile(0.25)
-Q3 = retail_raw['item_price'].quantile(0.75)
-IQR = 	Q3 - Q1
+# Check kolom yang memiliki missing data
+print('Check kolom yang memiliki missing data:')
+print(retail_raw.isnull().any())
 
-# Check ukuran (baris dan kolom) sebelum data yang outliers dibuang
-print('Shape awal: ', retail_raw.shape)
+# Filling the missing value (imputasi)
+print('\nFilling the missing value (imputasi):')
+print(retail_raw['quantity'].fillna(retail_raw.quantity.mean()))
 
-# Removing outliers
-retail_raw = retail_raw[~((retail_raw['item_price'] < (Q1 -1.5 * IQR)) | (retail_raw['item_price'] > (Q3 -1.5 * IQR)))]
-
-# Check ukuran (baris dan kolom) setelah data yang outliers dibuang
-print('Shape akhir: ', retail_raw.shape)
+# Drop missing value
+print('\nDrop missing value:')
+print(retail_raw['quantity'].dropna())
