9e7c7e4fe5d75486bfda3041ca158835d2542959
Yunita Sari <102893382+yunitasari193310027@users.noreply.github.com>
2023-03-06 19:42:37 +0700

    Create Descriptive_Statistics_part2.py
    count()

---
+++ Data Quality with Python for Beginner/Descriptive_Statistics_part2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import io
+import pandas_profiling
+retail_raw = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv')
+
+# Count kolom city
+count_city = retail_raw['city'].count()
+print('Count kolom count_city:', count_city)
+
+# Tugas praktek: count kolom product_id
+count_product_id = retail_raw['product_id'].count()
+print('Count kolom product_id:', count_product_id)
