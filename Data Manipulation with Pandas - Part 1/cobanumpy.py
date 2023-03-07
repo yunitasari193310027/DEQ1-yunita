854bb7888a0342c27525ae3dd5ede0feb66fd300
Yunita Sari <102893382+yunitasari193310027@users.noreply.github.com>
2023-03-07 10:00:35 +0700

    Update cobanumpy.py
    fix

--- Data Manipulation with Pandas - Part 1/cobanumpy.py
+++ Data Manipulation with Pandas - Part 1/cobanumpy.py
@@ -5,7 +5,7 @@ arr_series = np.array([1,2,3,4,5,6,6,7])
 ex_series = pd.Series(arr_series)
 print(ex_series)
 # Creating dataframe from numpy array (2D)
-arr_df = np.array([[1,2,3,4,5],
+arr_df = np.array([[1,2,3,5],
                    [5,6,7,8],
                    ['a','b','c',10]])
 ex_df = pd.DataFrame(arr_df)
