07dbfa6d6879c2c23c4c73e7235a35cb594d6649
Yunita Sari <102893382+yunitasari193310027@users.noreply.github.com>
2023-03-07 13:56:12 +0700

    Update read_dataset.py
    read json

--- Data Manipulation with Pandas - Part 1/read_dataset.py
+++ Data Manipulation with Pandas - Part 1/read_dataset.py
@@ -9,3 +9,8 @@ print(df_tsv.head(3)) # Menampilkan 3 data teratas
 # File xlsx dengan data di sheet "test"
 df_excel = pd.read_excel("https://storage.googleapis.com/dqlab-dataset/sample_excel.xlsx", sheet_name="test")
 print(df_excel.head(4)) # Menampilkan 4 data teratas
+
+# File JSON
+url = "https://storage.googleapis.com/dqlab-dataset/covid2019-api-herokuapp-v2.json"
+df_json = pd.read_json(url)
+print(df_json.head(10)) # Menampilkan 10 data teratas
