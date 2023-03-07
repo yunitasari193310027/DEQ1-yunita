b2899f3a998cd0e9b3b99a10dfa3510a4a121ab8
Yunita Sari <102893382+yunitasari193310027@users.noreply.github.com>
2023-03-07 13:53:52 +0700

    Update read_dataset.py
    add read excel

--- Data Manipulation with Pandas - Part 1/read_dataset.py
+++ Data Manipulation with Pandas - Part 1/read_dataset.py
@@ -5,3 +5,7 @@ print(df_csv.head(3)) # Menampilkan 3 data teratas
 # File TSV
 df_tsv = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_tsv.tsv", sep='\t')
 print(df_tsv.head(3)) # Menampilkan 3 data teratas
+
+# File xlsx dengan data di sheet "test"
+df_excel = pd.read_excel("https://storage.googleapis.com/dqlab-dataset/sample_excel.xlsx", sheet_name="test")
+print(df_excel.head(4)) # Menampilkan 4 data teratas
